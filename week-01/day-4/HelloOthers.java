63d94fc9a149f803ae3a500bac9b3292c948dae2
osziszab <osztertagszabolcs@gmail.com>
2019-01-17 17:57:11 +0100

    1...codes

---
+++ week-01/day-4/HelloOthers.java
@@ -0,0 +1,14 @@
+public class HelloOthers {
+    public static void main(String[] args) {
+        // Greet 3 of your classmates with this program, in three separate lines
+        // like:
+        //
+        System.out.println("Hello, Esther!");
+        System.out.println("Hello, Marry!");
+        System.out.println("Hello, Joe!");
+
+    }
+
+
+
+}
---
+++ week-01/day-4/TwoNumbers.java
@@ -0,0 +1,26 @@
+import com.sun.scenario.effect.impl.sw.sse.SSEBlend_SRC_OUTPeer;
+
+import java.sql.SQLOutput;
+
+public class TwoNumbers {
+
+    public static void main(String[] args) {
+        // Create a program that prints a few operations on two numbers: 22 and 13
+        System.out.println(13+22);
+        // Print the result of 13 added to 22
+        System.out.println(22-13);
+        // Print the result of 13 substracted from 22
+        System.out.println(22*13);
+        // Print the result of 22 multiplied by 13
+        System.out.println(22/13.);
+        // Print the result of 22 disubstractedvided by 13 (as a decimal fraction)
+        System.out.println(22/13);
+        // Print the integer part of 22 divided by 13
+        System.out.println(22%13);
+        // Print the remainder of 22 divided by 13
+
+    }
+
+
+
+}
