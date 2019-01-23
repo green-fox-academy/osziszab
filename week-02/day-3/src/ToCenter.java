c9fb8ab3cabb87793739f2851b414da29e43c1f5
osziszab <osztertagszabolcs@gmail.com>
2019-01-23 14:10:07 +0100

    2 more

---
+++ week-02/day-3/src/ToCenter.java
@@ -0,0 +1,55 @@
+import javax.swing.*;
+
+import java.awt.*;
+
+import static javax.swing.JFrame.EXIT_ON_CLOSE;
+public class ToCenter {
+    public static void mainDraw(Graphics graphics) {
+        // Create a line drawing function that takes 3 parameters:
+        // The x and y coordinates of the line's starting point and the graphics
+        // and draws a line from that point to the center of the canvas.
+        // Draw 3 lines with that function. Use loop for that.
+        int x=0;
+        int y=0;
+        lines(x,y,graphics);
+
+    }
+
+    // Don't touch the code below
+    static int WIDTH = 320;
+    static int HEIGHT = 320;
+
+    public static void main(String[] args) {
+        JFrame jFrame = new JFrame("Drawing");
+        jFrame.setDefaultCloseOperation(EXIT_ON_CLOSE);
+        ImagePanel panel = new ImagePanel();
+        panel.setPreferredSize(new Dimension(WIDTH, HEIGHT));
+        jFrame.add(panel);
+        jFrame.setLocationRelativeTo(null);
+        jFrame.setVisible(true);
+        jFrame.pack();
+    }
+
+    static class ImagePanel extends JPanel {
+        @Override
+        protected void paintComponent(Graphics graphics) {
+            super.paintComponent(graphics);
+            mainDraw(graphics);
+
+
+        }
+    }
+
+    public static void lines(int x, int y, Graphics g){
+
+        for (int i=0; i<3; i++){
+
+            g.drawLine(x+(i*30) , y, WIDTH/2, HEIGHT/2);
+        }
+
+
+    }
+
+
+
+}
