90ad59eccbe42f684c5954bb9ff6fe1e61d3507f
osziszab <osztertagszabolcs@gmail.com>
2019-01-23 15:55:19 +0100

    CenterBoxFunction

---
+++ week-02/day-3/src/CenterBoxFunction.java
@@ -0,0 +1,56 @@
+import javax.swing.*;
+
+import java.awt.*;
+
+import static javax.swing.JFrame.EXIT_ON_CLOSE;
+
+public class CenterBoxFunction {
+
+    public static void mainDraw(Graphics graphics){
+        // create a square drawing function that takes 2 parameter:
+        // the square size and the graphics
+        // and draws a square of that size to the center of the canvas.
+        // draw 3 squares with that function.
+        // avoid code duplication.
+            int x=50;
+            square(graphics,x);
+
+
+    }
+
+    // Don't touch the code below
+    static int WIDTH = 320;
+    static int HEIGHT = 320;
+
+    public static void main(String[] args) {
+        JFrame jFrame = new JFrame("Drawing");
+        jFrame.setDefaultCloseOperation(EXIT_ON_CLOSE);
+        ImagePanel panel = new ImagePanel();
+        panel.setPreferredSize(new Dimension(WIDTH, HEIGHT));
+        jFrame.add(panel);
+        jFrame.setLocationRelativeTo(null);
+        jFrame.setVisible(true);
+        jFrame.pack();
+    }
+
+    static class ImagePanel extends JPanel {
+        @Override
+        protected void paintComponent(Graphics graphics) {
+            super.paintComponent(graphics);
+            mainDraw(graphics);
+        }
+    }
+    public static void square(Graphics graphics, int x){
+
+        for (int i=0; i<3; i++){
+
+            graphics.setColor(Color.black);
+            graphics.drawRect(150-(x/2),150-(x/2),x,x);
+            x+=50;
+
+
+        }
+
+
+    }
+}
