18e4aaa3f468fc5fde5fc00135d8016fb9c6988a
osziszab <osztertagszabolcs@gmail.com>
2019-01-30 14:03:53 +0100

    Sharpie

---
+++ week-03/day-3/src/sharpie/Main.java
@@ -0,0 +1,9 @@
+package sharpie;
+
+public class Main {
+    public static void main(String[] args) {
+
+    Sharpie example = new Sharpie("red", 30);
+    example.use();
+    }
+}
---
+++ week-03/day-3/src/sharpie/Sharpie.java
@@ -0,0 +1,14 @@
+package sharpie;
+
+public class Sharpie {
+
+    String color;
+    float wildth;
+    static float inkAmount = 100;
+
+    public Sharpie(String color, float wildth){
+    }
+    public void use (){
+        inkAmount--;
+    }
+}
