925365d2c0bcebc10b30908f785a2233cf866678
osziszab <osztertagszabolcs@gmail.com>
2019-01-31 15:22:31 +0100

    power

---
+++ week-03/day-4/src/Power.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class Power {
+    public static void main(String[] args) {
+        // Given base and n that are both 1 or more, compute recursively (no loops)
+        // the value of base to the n power, so powerN(3, 2) is 9 (3 squared).
+
+        Scanner scanner = new Scanner(System.in);
+        int base = scanner.nextInt();
+        int n = scanner.nextInt();
+        System.out.println(power(base, n));
+    }
+    public static int power (int base, int n){
+        if (n != 2){
+            base =  power(base, n-1 );
+        }
+        base *= base;
+        return base;
+    }
+}
