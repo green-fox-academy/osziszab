c068386bdbd91122048062955207a76b79a71510
osziszab <osztertagszabolcs@gmail.com>
2019-01-31 15:42:33 +0100

    bunnies

---
+++ week-03/day-4/src/Bunny1.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class Bunny1 {
+    public static void main(String[] args) {
+        // We have a number of bunnies and each bunny has two big floppy ears.
+        // We want to compute the total number of ears across all the bunnies recursively (without loops or multiplication).
+
+        Scanner scanner = new Scanner(System.in);
+        int numberOfBunnies = scanner.nextInt();
+        System.out.println(countEars(numberOfBunnies));
+    }
+    public static int countEars(int bunnies){
+        int numberOfEars = 0;
+        if(bunnies > 0){
+            numberOfEars =(bunnies+bunnies);
+        }
+        return numberOfEars;
+    }
+}
