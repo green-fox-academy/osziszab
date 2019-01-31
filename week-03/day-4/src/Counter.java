324b62b2c2068bc3ce5a7a80cae2e57f133d33ef
osziszab <osztertagszabolcs@gmail.com>
2019-01-31 10:40:29 +0100

    Counter

---
+++ week-03/day-4/src/Counter.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class Counter {
+    public static void main(String[] args) {
+// Write a recursive function that takes one parameter: n and counts down from n.
+        Scanner scanner = new Scanner(System.in);
+        int n = scanner.nextInt();
+        counter(n);
+
+    }
+    public static void counter(int n) {
+        if (n >= 0) {
+            System.out.println(n);
+            counter(n - 1);
+        }
+    }
+}
