98fd590ef5d1bd3e7d5735257cba40fe21b1b0a6
osziszab <osztertagszabolcs@gmail.com>
2019-02-05 14:14:52 +0100

    some classes

---
+++ week-04/day-2/src/inheritance_exercise/Person.java
@@ -0,0 +1,27 @@
+package inheritance_exercise;
+
+public class Person {
+
+    String name;
+    int age;
+    String gender;
+
+    public Person(){
+        this.name = "Jane Doe";
+        this.age = 30;
+        this.gender = "female";
+    }
+    public Person (String name, int age, String gender){
+        this.name = name;
+        this.age = age;
+        this.gender = gender;
+    }
+
+    public  void introduce (Person person){
+        System.out.println("Hi, I'm " + name +", a " + age + "year old " + gender + ".");
+    }
+
+    public  void  getGoal(){
+        System.out.println("My goal is: Live for the moment!");
+    }
+}
---
+++ week-04/day-2/src/inheritance_exercise/Sponsor.java
@@ -0,0 +1,5 @@
+package inheritance_exercise;
+
+public class Sponsor {
+    
+}
