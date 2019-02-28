4b9557320fc3f5510a7d27e0e68a7a5bc230f0d5
osziszab <osztertagszabolcs@gmail.com>
2019-02-28 12:58:08 +0100

    list

--- week-06/day-4/src/main/java/com/greenfoxacademy/basicwebshop/controllers/MainController.java
+++ week-06/day-4/src/main/java/com/greenfoxacademy/basicwebshop/controllers/MainController.java
@@ -2,6 +2,9 @@ package com.greenfoxacademy.basicwebshop.controllers;
 
 import com.greenfoxacademy.basicwebshop.models.ShopItem;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -17,5 +20,14 @@ public class MainController {
         itemList.add(new ShopItem("Coca Cola","0,5l standard coke",25,0));
         itemList.add(new ShopItem("T-shirt","Bluew with a corgi ona a bike",300,1));
     }
+
+    @RequestMapping(value="/home")
+    public String allItem(Model model){
+        model.addAttribute("allItem",itemList );
+        return "index";
+    }
+
+  //  @RequestMapping(value="/home")
+
 
 }
