0a79978f08bfa2cd44a13a881e223437de480af9
osziszab <osztertagszabolcs@gmail.com>
2019-02-28 17:10:23 +0100

    max started

--- week-06/day-4/src/main/java/com/greenfoxacademy/basicwebshop/controllers/MainController.java
+++ week-06/day-4/src/main/java/com/greenfoxacademy/basicwebshop/controllers/MainController.java
@@ -20,6 +20,7 @@ public class MainController {
         itemList.add(new ShopItem("Running Shoes", "Nike running shoes for every day sport", 1000, 5));
         itemList.add(new ShopItem("Printer", "Some HP printer that will print pages", 3000, 2));
         itemList.add(new ShopItem("Coca Cola", "0,5l standard coke", 25, 0));
+        itemList.add(new ShopItem("Wokin","Chicken with fried rice and WOKIN sauce",119,100));
         itemList.add(new ShopItem("T-shirt", "Bluew with a corgi ona a bike", 300, 1));
     }
 
@@ -58,11 +59,18 @@ public class MainController {
     @RequestMapping(value = "average-stock")
     public String avrageStock(Model model) {
         double avrage = itemList.stream()
-                .mapToInt(s -> s.getQuantityOfStock())
+                .mapToInt(ShopItem::getQuantityOfStock)
                 .summaryStatistics()
                 .getAverage();
-        model.addAttribute("avrageStock", "Average stock" + avrage);
+        model.addAttribute("avrageStock", avrage);
         return "avrage";
     }
 
+    @RequestMapping(value = "most-expensive")
+    public String mostExpensive(Model model){
+        model.addAttribute("mostexpensive", itemList.stream()
+                .sorted(Comparator.comparing(ShopItem::getPrice)
+                ).collect(Collectors.toList()));
+        return "expensive";
+    }
 }
