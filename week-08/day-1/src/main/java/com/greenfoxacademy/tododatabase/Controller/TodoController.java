e60df1e6e64137c6b23b2aa2ba9a3552b1edda90
osziszab <osztertagszabolcs@gmail.com>
2019-03-11 11:55:34 +0100

    added list method

--- week-08/day-1/src/main/java/com/greenfoxacademy/tododatabase/Controller/TodoController.java
+++ week-08/day-1/src/main/java/com/greenfoxacademy/tododatabase/Controller/TodoController.java
@@ -1,4 +1,16 @@
 package com.greenfoxacademy.tododatabase.Controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+@RequestMapping(value = "/todo")
 public class TodoController {
+
+    @RequestMapping(value = {"/","/list"})
+    @ResponseBody
+    public String list(){
+        return "This is my first Todo";
+    }
 }
