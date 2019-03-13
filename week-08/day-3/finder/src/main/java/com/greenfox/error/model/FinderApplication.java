e1372b7eb02b692894e1921384c9b7970489240c
osziszab <osztertagszabolcs@gmail.com>
2019-03-13 17:55:37 +0100

    connectd with mysql

--- week-08/day-3/finder/src/main/java/com/greenfox/error/model/FinderApplication.java
+++ week-08/day-3/finder/src/main/java/com/greenfox/error/model/FinderApplication.java
@@ -1,12 +1,18 @@
 package com.greenfox.error.model;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class FinderApplication {
+public class FinderApplication implements CommandLineRunner {
 
 	public static void main(String[] args) {
 		SpringApplication.run(FinderApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+
 	}
 }
