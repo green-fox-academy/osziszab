43e3a131a076be31a439d352d5ad6166a7add86e
osziszab <osztertagszabolcs@gmail.com>
2019-03-13 15:44:19 +0100

    working index

---
+++ week-08/day-3/finder/src/main/java/com/greenfox/error/model/controller/AppController.java
@@ -0,0 +1,44 @@
+package com.greenfox.error.model.controller;
+
+import org.springframework.web.bind.annotation.ModelAttribute;
+import com.greenfox.error.model.service.UserService;
+import com.greenfox.error.model.finder.User;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ * Created by aze on 25/10/17.
+ */
+@Controller
+@RequestMapping("/app")
+public class AppController {
+
+    private UserService service;
+
+    @Autowired
+    public AppController(UserService service) {
+        this.service = service;
+    }
+
+    @GetMapping("/")
+    public String index(Model model) {
+        model.addAttribute("user", new User());
+        model.addAttribute("yolo", service.getAll());
+        return "index";
+    }
+
+    @GetMapping("/app")
+    public String create(@ModelAttribute User user) {
+        service.save(user);
+        return "redirect:/";
+    }
+
+    @PostMapping("/app")
+    public String createUser() {
+        return "redirect:/";
+    }
+}
---
+++ week-08/day-3/finder/src/main/java/com/greenfox/error/model/finder/User.java
@@ -0,0 +1,36 @@
+package com.greenfox.error.model.finder;
+
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+
+/**
+ * Created by aze on 25/10/17.
+ */
+
+public class User {
+    private String firstName;
+    private String lastName;
+
+    public User(String firstName, String lastName) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+    }
+    public User(){
+
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+}
