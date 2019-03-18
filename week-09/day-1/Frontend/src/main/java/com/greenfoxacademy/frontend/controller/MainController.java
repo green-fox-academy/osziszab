d262ddc122eb386f0ee7408edeed1a9b8c063385
osziszab <osztertagszabolcs@gmail.com>
2019-03-18 13:39:19 +0100

    index.html and frontend.js added

--- week-09/day-1/Frontend/src/main/java/com/greenfoxacademy/frontend/controller/MainController.java
+++ week-09/day-1/Frontend/src/main/java/com/greenfoxacademy/frontend/controller/MainController.java
@@ -1,4 +1,13 @@
 package com.greenfoxacademy.frontend.controller;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class MainController {
+
+    @GetMapping(value = "/")
+    public String mainPage(){
+        return "index";
+    }
 }
