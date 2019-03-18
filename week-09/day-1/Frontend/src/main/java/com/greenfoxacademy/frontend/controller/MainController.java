b6510b91a66768dad9294ef004b76df12d060f61
osziszab <osztertagszabolcs@gmail.com>
2019-03-18 13:47:30 +0100

    restController added

--- week-09/day-1/Frontend/src/main/java/com/greenfoxacademy/frontend/controller/MainController.java
+++ week-09/day-1/Frontend/src/main/java/com/greenfoxacademy/frontend/controller/MainController.java
@@ -1,13 +1,15 @@
 package com.greenfoxacademy.frontend.controller;
 
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController
+@Controller
 public class MainController {
 
     @GetMapping(value = "/")
     public String mainPage(){
         return "index";
     }
+
 }
---
+++ week-09/day-1/Frontend/src/main/java/com/greenfoxacademy/frontend/controller/MainRestController.java
@@ -0,0 +1,7 @@
+package com.greenfoxacademy.frontend.controller;
+
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class MainRestController {
+}
