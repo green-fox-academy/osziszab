22cc2788d3bb4a8532005b538ff6fa27a46a444e
osziszab <osztertagszabolcs@gmail.com>
2019-04-03 15:04:11 +0200

    File IO copy from to file another one

--- week-10/day-3/ConsoleApp1/ConsoleApp1/Program.cs
+++ week-10/day-3/ConsoleApp1/ConsoleApp1/Program.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading;
 using System.Threading.Tasks;
+using System.IO;
 
 namespace ConsoleApp1
 {
@@ -11,11 +12,35 @@ namespace ConsoleApp1
     {
         static void Main(string[] args)
         {
-
-            PlayMainTheme();
+            String Path = @"C:\Users\Szabi\greenfox\osziszab\week-10\day-3\ConsoleApp1\ConsoleApp1\SW.txt";
+            bool isSucces = CopyFileToAnother(Path);
+            Console.WriteLine("Result of copying: " + isSucces);
         }
 
-        
+        public static bool CopyFileToAnother(String originalPath) {
+            bool result;
+            string targetPath = @"C:\Users\Szabi\greenfox\osziszab\week-10\day-3\ConsoleApp1\ConsoleApp1\newFile.txt";
+            List<string> lines = new List<string>();
+            List<string> newFilesLines = new List<string>();
+            try{
+                lines = File.ReadAllLines(originalPath).ToList<string>();
+                File.WriteAllLines(targetPath, lines);
+                newFilesLines = File.ReadAllLines(targetPath).ToList<string>();                
+            }
+
+            catch (Exception ex){
+                Console.WriteLine(ex.Message);
+            }
+
+            if (newFilesLines != null){
+                result = true;
+            }else{
+                result = false;
+            }
+            //lines.ForEach(Console.WriteLine);       
+            return result;
+        }
+
        public static void PlayMainTheme()
         {
             Console.Beep(300, 500);
